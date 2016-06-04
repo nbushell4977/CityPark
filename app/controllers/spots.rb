93d6cd8d53071a65ecd2c03622367656982f9193
Shirley Rozenboim <srozenboim@gmail.com>
2016-06-04 16:26:12 -0700

    Added a search form on the home page. It's currently not doing anything except for redirecting to the results page

--- app/controllers/spots.rb
+++ app/controllers/spots.rb
@@ -1,3 +1,12 @@
 get '/spots/new' do 
   erb :'/spots/new'
 end
+
+get '/spots' do
+  erb :'/spots/show'
+end
+
+post '/spots' do
+  @spot = ParkingSpot.find_by_zip(params[:zip])
+  redirect '/spots'
+end
