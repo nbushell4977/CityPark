c97ad09490d1b695ebc239917973557c02606b61
Shirley Rozenboim <srozenboim@gmail.com>
2016-06-04 23:50:09 -0700

    a user can now search for parking spots near a specific address. also formatted the results page so it shows the address of the parking spot, the username of the user who crated it, and when the posting was created

--- app/controllers/spots.rb
+++ app/controllers/spots.rb
@@ -9,27 +9,26 @@ end
 def address
   address_string = ""
   if params[:address]
-    address_string += params[:address]+" "
+    address_string += params[:address]+", "
   end
   if params[:city]
-    address_string += params[:city]+" "
+    address_string += params[:city]+", "
   end
   if params[:state]
     address_string += params[:state]+" "
   end
   if params[:zip]
-    address_string += params[:zip]+" "
+    address_string += params[:zip]
   end
+  address_string
 end
 
 post '/spots/results' do
-  @spots = ParkingSpot.near(address)
+  @spots = ParkingSpot.near(address,10)
   erb :'/spots/index'
 end
 
 post '/spots' do
-  p params
-  p "*" * 1000
   user = User.find(session[:id])
   @spot = ParkingSpot.new(params)
   @spot.user_id = user.id
