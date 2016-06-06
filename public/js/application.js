9f6b2ab8b10385ee99eedd557ede7bb87e24f56b
Shirley Rozenboim <srozenboim@gmail.com>
2016-06-06 12:35:19 -0700

    a user can click on a spot posting and get to a page that displays info about that posting

--- public/js/application.js
+++ public/js/application.js
@@ -1,8 +1,9 @@
 function initMap() {
-  var myLatLng = {lat: 37.7576793, lng: -122.5076402};
+  // var myLatLng = {lat: 37.7576793, lng: -122.5076402};
 
-  // var centerInfo = getPinInfo($("#search_results").first.find('li').first);
-  // var myLatLng = {lat: centerInfo[0], lng: centerInfo[1]};
+  var centerInfo = getPinInfo($("#search_results").first().find('li').first());
+  var myLatLng = {lat: centerInfo[0], lng: centerInfo[1]};
+
 
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 10,
@@ -14,21 +15,39 @@ function initMap() {
 
 parkingSpots = []
 
+// function getPinInfo(element) {
+// 		var lat = $(element).children(".latitude").text();
+// 		var long = $(element).children(".longitude").text();
+// 		var parsed_lat = parseFloat(lat);
+// 		var parsed_long = parseFloat(long);
+// 		parkingSpots.push([parsed_lat, parsed_long]);
+// };
+
+// function createMarkerArray() {
+// 	$("#search_results").each(function() {
+// 		$(this).find('li').each(function(){
+//       getPinInfo(this);
+// 		});
+// 	});
+// 	return parkingSpots;
+// };
+
 function getPinInfo(element) {
-		var lat = $(element).children(".latitude").text();
-		var long = $(element).children(".longitude").text();
-		var parsed_lat = parseFloat(lat);
-		var parsed_long = parseFloat(long);
-		parkingSpots.push([parsed_lat, parsed_long]);
+        var lat = $(element).children(".latitude").text();
+        var long = $(element).children(".longitude").text();
+        var parsed_lat = parseFloat(lat);
+        var parsed_long = parseFloat(long);
+        return [parsed_lat, parsed_long];
 };
 
+//this should push pinInfo onto parkingSpots
 function createMarkerArray() {
-	$("#search_results").each(function() {
-		$(this).find('li').each(function(){
-      getPinInfo(this);
-		});
-	});
-	return parkingSpots;
+    $("#search_results").each(function() {
+        $(this).find('li').each(function(){
+      parkingSpots.push(getPinInfo(this));
+        });
+    });
+    return parkingSpots;
 };
 
 function setMarkers(map) {
