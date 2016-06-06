2aee2b4b6a3ede88dd4a9e46472ae2c47a7fa7f3
Shirley Rozenboim <srozenboim@gmail.com>
2016-06-06 12:10:46 -0700

    the search results now show up as markers on the map

--- public/js/application.js
+++ public/js/application.js
@@ -1,29 +1,45 @@
 function initMap() {
-  var myLatLng = {lat: -25.363, lng: 131.044};
+  var myLatLng = {lat: 37.7576793, lng: -122.5076402};
+
+  // var centerInfo = getPinInfo($("#search_results").first.find('li').first);
+  // var myLatLng = {lat: centerInfo[0], lng: centerInfo[1]};
 
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 10,
     center: myLatLng
   });
 
-  // setMarkers(map);
+  setMarkers(map);
 };
 
 parkingSpots = []
 
-function getPinInfo() {
-		var lat = $("#latitude").text();
-		var long = $("#longitude").text();
+function getPinInfo(element) {
+		var lat = $(element).children(".latitude").text();
+		var long = $(element).children(".longitude").text();
 		var parsed_lat = parseFloat(lat);
 		var parsed_long = parseFloat(long);
-		parkingSpots.push(parsed_lat, parsed_long)
-}
+		parkingSpots.push([parsed_lat, parsed_long]);
+};
 
-function formMarkerArray() {
+function createMarkerArray() {
 	$("#search_results").each(function() {
 		$(this).find('li').each(function(){
-     getPinInfo();
+      getPinInfo(this);
 		});
 	});
-	return parkingSpots
+	return parkingSpots;
+};
+
+function setMarkers(map) {
+  createMarkerArray();
+   console.log(parkingSpots)
+  for (var i = 0; i < parkingSpots.length; i++) {
+    var spot = parkingSpots[i];
+    var marker = new google.maps.Marker({
+      position: {lat: spot[0], lng: spot[1]},
+      map: map,
+      title: "spot"
+    });
+  };
 };
